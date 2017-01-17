795da09971911f4f05361b7cadf88e38dc416e05
adrianadelnero <adriana.delnero@gmail.com>
2017-01-17 18:07:34 -0200

    Strategy

---
+++ src/main/java/br/com/designpatterns/strategy/CalculadorDeImpostos.java
@@ -0,0 +1,11 @@
+package br.com.designpatterns.strategy;
+
+public class CalculadorDeImpostos {
+	
+    public void realizaCalculo(Orcamento orcamento, Imposto imposto) {
+
+        double valor = imposto.calcula(orcamento); 
+
+        System.out.println(valor);
+      }
+}
